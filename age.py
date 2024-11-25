213a43e447ddb58039d1fb48a731c712bb3aa1ca
ayan123056 <jasirafowaz@yahoo.com>
2024-11-25 20:03:14 +0400

    initial commit

---
+++ age.py
@@ -0,0 +1,7 @@
+Age = int(input("Enter your age"))
+if Age >= 20:
+    print("You cannot enter the class as youre too old")
+elif Age <= 10:
+    print("You cant enter the class as youre too young")
+else:
+    print("You are enrolled into the class")
